453c342ad36209ee5d2e0b2b9cd766a7bde0a3ac
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-06-19 18:29:25 +0900

    add: textAnimation

--- src/scene/MainScene.ts
+++ src/scene/MainScene.ts
@@ -10,6 +10,9 @@ export class MainScene extends Phaser.Scene {
   countEffectText: Phaser.GameObjects.Text | null = null;
   countWeight: any;
 
+  enemyHp: any;
+  enemyHpText: Phaser.GameObjects.Text | null = null;
+
   constructor() {
     super("main");
   }
@@ -18,6 +21,7 @@ export class MainScene extends Phaser.Scene {
     this.load.image("street", "../assets/img/background/street.png");
     this.load.image("dragon", "../assets/img/enemy/dragon.png");
     this.load.audio("bossBattleBGM", "../assets/bgm/bossBGM_vsDragon.mp3");
+    this.load.image("spark", "../assets/img/enemy/smallDragonA.png");
   }
 
   create() {
@@ -28,6 +32,14 @@ export class MainScene extends Phaser.Scene {
       this.count = 0;
     }
     this.countWeight = 1; //countの重み
+
+    //敵の体力
+    const storedEnemyStatus = localStorage.getItem("enemyHp");
+    if (storedEnemyStatus) {
+      this.enemyHp = JSON.parse(storedEnemyStatus);
+    } else {
+      this.enemyHp = 1000; //体力の初期値
+    }
 
     const playerStatusTextStyle: Phaser.Types.GameObjects.Text.TextStyle = {
       fontFamily: "Arial, sans-serif",
@@ -81,7 +93,16 @@ export class MainScene extends Phaser.Scene {
     this.add.image(width / 2, height / 2 - 200, "dragon").setScale(5.0);
 
     this.countText = this.add
-      .text(width / 2, 50, "Money " + this.count.toString(),textStyle)
+      .text(width / 2, 50, "Money " + this.count.toString(), textStyle)
+      .setOrigin(0.5)
+      .setScale(2.0);
+    this.enemyHpText = this.add
+      .text(
+        width / 2,
+        height / 2 + 100,
+        "HP " + this.enemyHp.toString(),
+        textStyle
+      )
       .setOrigin(0.5)
       .setScale(2.0);
 
@@ -92,94 +113,119 @@ export class MainScene extends Phaser.Scene {
 
     zone.on("pointerdown", () => {
       this.count = this.count + this.countWeight; //クリック数を増やす
-      if (this.countText) {
+      this.enemyHp = this.enemyHp - this.countWeight;
+      if (this.countText) { //nullチェック
         this.countText.setText("Money " + this.count.toString()); // テキストオブジェクトを更新
       }
+      if (this.enemyHpText) { //nullチェック
+        this.enemyHpText.setText("HP " + this.enemyHp.toString()); // テキストオブジェクトを更新
+      }
+
       localStorage.setItem("count", JSON.stringify(this.count)); // localStrogeの更新
+      localStorage.setItem("enemyHp", JSON.stringify(this.enemyHp)); // localStrogeの更新
     });
 
     const clickEventAnimationText: Phaser.Types.GameObjects.Text.TextStyle = {
       fontFamily: "Arial",
       fontSize: "68px",
-      fontStyle: "bold", // フォントスタイルを太字に設定
+      //fontStyle: "bold", // フォントスタイルを太字に設定
       color: "#000000",
       //stroke: '#000000', // テキストの輪郭色を黒に設定
       strokeThickness: 10, // テキストの輪郭の太さを設定
-      shadow: { // テキストの影を設定
+      shadow: {
+        // テキストの影を設定
         offsetX: 5, // 水平方向のオフセット
         offsetY: 5, // 垂直方向のオフセット
-        //color: '#333333', // 影の色
+        color: '#00ff00', // 影の色
         blur: 5, // 影のぼかし度合い
         stroke: true, // 影に輪郭を適用するかどうか
         fill: true, // 影に塗り潰しを適用するかどうか
       },
-      align: 'center', // テキストの水平方向の位置合わせ（left, center, right）
+      align: "center", // テキストの水平方向の位置合わせ（left, center, right）
       //baseline: 'alphabetic', // テキストの垂直方向の位置合わせ（top, hanging, middle, alphabetic, ideographic, bottom）
-      wordWrap: { // 単語の折り返しを設定
+      wordWrap: {
+        // 単語の折り返しを設定
         width: 300, // 折り返す幅
         useAdvancedWrap: true, // 改行やハイフンを適切に処理する高度な折り返し機能を使用
-      }
+      },
     };
 
     //クリックイベントの設定
     zone.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
-      const EffectText = this.add
-        .text(pointer.x, pointer.y, this.countWeight.toString(), clickEventAnimationText)
-        .setOrigin(0.5);
-    
-      // スケールアニメーション
-      this.tweens.add({
-        targets: EffectText,
-        scaleX: 2, // X方向のスケール倍率
-        scaleY: 2, // Y方向のスケール倍率
-        duration: 1000,
-        ease: "Bounce.easeOut", // イージング関数の変更
-      });
-    
-      // 回転アニメーション
-      this.tweens.add({
-        targets: EffectText,
-        angle: 360, // 回転角度 (度)
-        duration: 1000,
-        ease: "Cubic.easeInOut",
-      });
-    
-      // Y軸方向の移動アニメーション
-      this.tweens.add({
-        targets: EffectText,
-        y: '-=100', // Y軸方向に100ピクセル上へ移動
-        duration: 1000,
-        ease: "Quadratic.easeOut",
-      });
-    
-      // カラーアニメーション
-      this.tweens.addCounter({
-        from: 0,
-        to: 100,
-        duration: 1000,
-        onUpdate: (tween) => {
-          const value = Phaser.Math.RoundTo(tween.getValue(), -1);
-          EffectText.setColor(Phaser.Display.Color.Interpolate.ColorWithColor(
-            Phaser.Display.Color.ValueToColor('#00FFFF'),
-            Phaser.Display.Color.ValueToColor('#00FF00'),
-            100,
-            value
-          ).toString());
-        },
-      });
-    
-      // フェードアウトアニメーション
-      this.tweens.add({
-        targets: EffectText,
-        alpha: 0,
-        delay: 500, // アニメーションを開始する前の遅延時間（500ms）
-        duration: 1000,
-        ease: "Linear",
-        onComplete: () => {
-          EffectText.destroy();
-        },
-      });
+      createEffectTextAnimation(
+        pointer.x,
+        pointer.y,
+        this.countWeight,
+        clickEventAnimationText,
+        this
+      );
     });
-    
   }
+}
+
+function createEffectTextAnimation(
+  x: number,
+  y: number,
+  countWeight: any,
+  clickEventAnimationText: Phaser.Types.GameObjects.Text.TextStyle,
+  scene: any
+) {
+  const EffectText = scene.add
+    .text(x, y, countWeight.toString(), clickEventAnimationText)
+    .setOrigin(0.5);
+
+  // スケールアニメーション
+  scene.tweens.add({
+    targets: EffectText,
+    scaleX: 2,
+    scaleY: 2,
+    duration: 1000,
+    ease: "Bounce.easeOut",
+  });
+
+  // 回転アニメーション
+  scene.tweens.add({
+    targets: EffectText,
+    angle: 360,
+    duration: 1000,
+    ease: "Cubic.easeInOut",
+  });
+
+  // Y軸方向の移動アニメーション
+  scene.tweens.add({
+    targets: EffectText,
+    y: "-=100",
+    duration: 1000,
+    ease: "Quadratic.easeOut",
+  });
+
+  // カラーアニメーション
+  scene.tweens.addCounter({
+    from: 0,
+    to: 100,
+    duration: 1000,
+    onUpdate: (tween: any) => {
+      const value = Phaser.Math.RoundTo(tween.getValue(), -1);
+      EffectText.setColor(
+        Phaser.Display.Color.Interpolate.ColorWithColor(
+          Phaser.Display.Color.ValueToColor("#00FFFF"),
+          Phaser.Display.Color.ValueToColor("#00FF00"),
+          100,
+          value
+        ).toString()
+      );
+    },
+  });
+
+  // フェードアウトアニメーション
+  scene.tweens.add({
+    targets: EffectText,
+    alpha: 0,
+    delay: 500,
+    duration: 1000,
+    ease: "Linear",
+    onComplete: () => {
+      EffectText.destroy();
+    },
+  });
 }
